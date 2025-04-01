790b3c6c0f32bf7f5aea3ccd13220bcddf9013e4
LeeDongjae <leedongjae625@gmail.com>
2025-04-01 17:52:14 +0900

    init: 프로젝트 생성

---
+++ src/main/java/com/ema/ema_backend/EmaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ema.ema_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ema/ema_backend/EmaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ema.ema_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
