65575a0a3ee0758577bbb07f262f33b01ff842ab
LeeDongjae <leedongjae625@gmail.com>
2025-04-02 11:32:53 +0900

    feat: security.config, web.config 작성

---
+++ src/main/java/com/ema/ema_backend/global/config/SecurityConfig.java
@@ -0,0 +1,43 @@
+package com.ema.ema_backend.global.config;
+
+import com.ema.ema_backend.domain.auth.jwt.JwtFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final JwtFilter jwtFilter;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                // ✅ CSRF 비활성화 (JWT는 세션이 없기 때문에)
+                .csrf(AbstractHttpConfigurer::disable)
+
+                // ✅ 세션 정책을 Stateless로 설정
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+                // ✅ URL별 인증 설정
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/api/auth/oauth/kakao/login"
+                        ).permitAll() // 인증 없이 접근 가능
+
+                        .anyRequest().authenticated() // 그 외는 인증 필요
+                )
+
+                // ✅ JwtFilter를 UsernamePasswordAuthenticationFilter 전에 삽입
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+}
