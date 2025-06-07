a44af401cc329ff8926334880a845d2c9598d0b2
LeeDongjae <leedongjae625@gmail.com>
2025-06-07 22:07:50 +0900

    FEAT: 이미지 포함한 채팅방 조회 개발

---
+++ src/main/java/com/ema/ema_backend/domain/chatroom/dto/PyPostChatImgRequest.java
@@ -0,0 +1,4 @@
+package com.ema.ema_backend.domain.chatroom.dto;
+
+public record PyPostChatImgRequest(String query, String image_base64) {
+}
--- src/main/java/com/ema/ema_backend/domain/chatroom/service/ChatRoomService.java
+++ src/main/java/com/ema/ema_backend/domain/chatroom/service/ChatRoomService.java
@@ -6,8 +6,6 @@ import com.ema.ema_backend.domain.chatroom.repository.ChatRoomRepository;
 import com.ema.ema_backend.domain.member.entity.Member;
 import com.ema.ema_backend.domain.member.service.MemberService;
 import com.ema.ema_backend.domain.message.Message;
-import com.ema.ema_backend.domain.message.dto.MessageSet;
-import com.ema.ema_backend.domain.message.repository.MessageRepository;
 import com.ema.ema_backend.domain.message.service.MessageService;
 import com.ema.ema_backend.global.exception.NotFoundException;
 import com.ema.ema_backend.global.exception.RemoteApiException;
@@ -19,8 +17,11 @@ import org.springframework.http.*;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Base64;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -48,13 +49,13 @@ public class ChatRoomService {
         ChatRoom chatRoom = new ChatRoom("Initial Chat", member);
         chatRoomRepository.save(chatRoom);
 
-        Message userMessage = messageService.createMessage("사용자", req.content(), chatRoom);
+        Message userMessage = messageService.createTextMessage("사용자", req.content(), chatRoom);
         chatRoom.getMessages().add(userMessage);
 
         // RestTemplate 통해 파이썬 서버 연결
         PyPostChatFirstResponse response = postFirstChat(new PyPostChatRequest(req.content()));
 
-        Message aiMessage = messageService.createMessage("공학수학 어시스턴스", response.answer(), chatRoom);
+        Message aiMessage = messageService.createTextMessage("공학수학 어시스턴스", response.answer(), chatRoom);
         chatRoom.getMessages().add(aiMessage);
 
         chatRoom.setRoomTitle(response.title());
@@ -108,13 +109,13 @@ public class ChatRoomService {
             throw new UnauthorizedAccessException("ChatRoom", "at ChatRoomService - postChat()");
         }
 
-        Message userMessage = messageService.createMessage("사용자", req.content(), chatRoom);
+        Message userMessage = messageService.createTextMessage("사용자", req.content(), chatRoom);
         chatRoom.getMessages().add(userMessage);
 
         // RestTemplate 통해 파이썬 서버 연결
         PyPostChatResponse response = postChat(new PyPostChatRequest(req.content()));
 
-        Message aiMessage = messageService.createMessage("공학수학 어시스턴스", response.answer(), chatRoom);
+        Message aiMessage = messageService.createTextMessage("공학수학 어시스턴스", response.answer(), chatRoom);
         chatRoom.getMessages().add(aiMessage);
 
         // FirstChatResponse 데이터 조립
@@ -241,5 +242,71 @@ public class ChatRoomService {
         }
 
         return new ResponseEntity<>(new ChatRoomInfoResponse(responseList), HttpStatus.OK);
+    }
+
+    @Transactional
+    public ResponseEntity<ChatResponse> postChatImg(Long ChatRoomId, MultipartFile file, String req, Authentication authentication) {
+        Optional<Member> optionalMember = memberService.checkPermission(authentication);
+        if (optionalMember.isEmpty()){
+            throw new NotFoundException("Member", "at ChatRoomService - postChat()");
+        }
+        Member member = optionalMember.get();
+
+        Optional<ChatRoom> optionalChatRoom = chatRoomRepository.findById(ChatRoomId);
+        if (optionalChatRoom.isEmpty()){
+            throw new NotFoundException("ChatRoom", "at ChatRoomService - postChat()");
+        }
+        ChatRoom chatRoom = optionalChatRoom.get();
+
+        // 유저의 chatroom이 맞는지 검증
+        if (!chatRoom.getMember().equals(member)){
+            throw new UnauthorizedAccessException("ChatRoom", "at ChatRoomService - postChat()");
+        }
+
+        // Base64 인코더로 인코딩
+        String dataUri = null;
+        try {
+            String base64 = Base64.getEncoder().encodeToString(file.getBytes());
+            dataUri = "data:" + file.getContentType() + ";base64," + base64;
+        } catch (IOException e) {
+            throw new RuntimeException("이미지 I/O Exception", e);
+        }
+
+        Message userMessage = messageService.createImgMessage("사용자", dataUri, req, chatRoom);
+        chatRoom.getMessages().add(userMessage);
+
+        // RestTemplate 통해 파이썬 서버 연결
+        PyPostChatResponse response = postChatImg(new PyPostChatImgRequest(req, dataUri));
+
+        Message aiMessage = messageService.createTextMessage("공학수학 어시스턴스", response.answer(), chatRoom);
+        chatRoom.getMessages().add(aiMessage);
+
+        // FirstChatResponse 데이터 조립
+        return new ResponseEntity<>(new ChatResponse(chatRoom.getId(), aiMessage.getSenderType().toString(), response.answer(), aiMessage.getCreatedAt()), HttpStatus.OK);
+    }
+
+    @Transactional
+    public PyPostChatResponse postChatImg(PyPostChatImgRequest req){
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        // HttpEntity에 바디와 헤더 담기
+        HttpEntity<PyPostChatImgRequest> httpEntity = new HttpEntity<>(req, headers);
+
+        // RestTemplate으로 POST 요청 보내기
+        ResponseEntity<PyPostChatResponse> responseEntity = restTemplate.postForEntity(
+                baseUri + "/qnaimg",
+                httpEntity,
+                PyPostChatResponse.class
+        );
+
+        // HTTP 상태 코드 체크 (200 OK 등)
+        if (responseEntity.getStatusCode() == HttpStatus.OK) {
+            // 8) 바디에 담긴 QuestionResponse 객체 꺼내기
+            return responseEntity.getBody();
+        } else {
+            // 오류 응답 처리 (예외 던지기 등)
+            throw new RemoteApiException("원격 서버 응답 실패: HTTP " + responseEntity.getStatusCode() + "," + baseUri + "/qua");
+        }
     }
 }
